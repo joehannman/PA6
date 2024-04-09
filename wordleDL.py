0b9a416d45537c77dc6fb7dd36f6c7ee12876b66
joehannman <142887121+joehannman@users.noreply.github.com>
2024-04-09 10:50:56 +0000

    data gaman
    data gaman

--- wordleDL.py
+++ wordleDL.py
@@ -1,15 +1,18 @@
 import random
 import csv
 class WordleDL:
-    def load_all_users():
-        pass
-
     def load_all_words(self):
         with open("word-bank.txt") as file:
             return [word.strip() for word in file.readlines()]
 
     def load_scores():
-        pass
+        ret_dict = {}
+        with open('scores.csv') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                ret_dict[row['userId']] = row['score']
+
+        return ret_dict
 
     def load_all_users(self):
         ret_dict = {}
@@ -20,8 +23,14 @@ class WordleDL:
 
         return ret_dict
     
-    def save_new_score():
-        pass
+    def save_new_score(userId, score):
+        with open('users.csv', 'a', newline='', encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['userId','score'])
+            
+            writer.writerow({
+                'userId': userId,
+                'score': score
+            })
 
     def save_user(self, userId, username):
         with open('users.csv', 'a', newline='', encoding="utf-8") as csvfile:
