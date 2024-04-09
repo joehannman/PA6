c0106184b05798e2f7fd70699dc44f2286a59aee
Guðni Hrafn <gudnihrafn93@gmail.com>
2024-04-09 13:29:28 +0000

    Worlde front end!

--- Gudnafallegt/wordle.py
+++ Gudnafallegt/wordle.py
@@ -8,97 +8,6 @@ SHOW_WORD = False
 USE_TEST_WORD = False
 TEST_WORD = "brain"
 
-
-class Alphabet:
-    def __init__(self, language="english") -> None:
-        self.english = {"A": "A", 
-                        "B": "B", 
-                        "C": "C", 
-                        "D": "D", 
-                        "E": "E",
-                        "F": "F", 
-                        "G": "G", 
-                        "H": "H", 
-                        "I": "I", 
-                        "J": "J",
-                        "K": "K", 
-                        "L": "L", 
-                        "M": "M", 
-                        "N": "N", 
-                        "O": "O",
-                        "P": "P", 
-                        "Q": "Q", 
-                        "R": "R", 
-                        "S": "S", 
-                        "T": "T",
-                        "U": "U", 
-                        "V": "V", 
-                        "W": "W", 
-                        "X": "X", 
-                        "Y": "Y",
-                        "Z": "Z"}
-        self.icelandic = {"A": "A",
-                            "Á": "Á",
-                            "B": "B", 
-                            "C": "C", 
-                            "D": "D",
-                            "Ð": "Ð", 
-                            "E": "E",
-                            "É": "É",
-                            "F": "F", 
-                            "G": "G", 
-                            "H": "H", 
-                            "I": "I",
-                            "Í": "Í", 
-                            "J": "J",
-                            "K": "K", 
-                            "L": "L", 
-                            "M": "M", 
-                            "N": "N", 
-                            "O": "O",
-                            "Ó": "Ó",
-                            "P": "P", 
-                            "Q": "Q", 
-                            "R": "R", 
-                            "S": "S", 
-                            "T": "T",
-                            "U": "U", 
-                            "Ú": "Ú",
-                            "V": "V", 
-                            "W": "W", 
-                            "X": "X", 
-                            "Y": "Y",
-                            "Ý": "Ý",
-                            "Z": "Z",
-                            "Þ": "Þ"}
-        self.language = language
-        language_dict = {"english": self.english, "icelandic": self.icelandic}
-        self.a = language_dict[self.language]
-
-    def set_green(self, letter):
-        self.a[letter.upper()] = f"\033[32m{letter.upper()}\033[0m"
-
-    def set_yellow(self, letter):
-        if self.a[letter.upper()] != f"\033[32m{letter.upper()}\033[0m":
-            self.a[letter.upper()] = f"\033[33m{letter.upper()}\033[0m"
-
-    def set_black(self, letter):
-        self.a[letter.upper()] = f"\033[30m{letter.upper()}\033[0m"
-            
-    def get_keyboard(self):
-        eng_kbd = f"""{SPACING}{self.a["Q"]} {self.a["W"]} {self.a["E"]} {self.a["R"]} {self.a["T"]} {self.a["Y"]} {self.a["U"]} {self.a["I"]} {self.a["O"]} {self.a["P"]}
-{SPACING} {self.a["A"]} {self.a["S"]} {self.a["D"]} {self.a["F"]} {self.a["G"]} {self.a["H"]} {self.a["J"]} {self.a["K"]} {self.a["L"]}
-{SPACING}  {self.a["Z"]} {self.a["X"]} {self.a["C"]} {self.a["V"]} {self.a["B"]} {self.a["N"]} {self.a["M"]}"""
-        ice_kbd = f"""{SPACING}{self.a["Q"]} {self.a["W"]} {self.a["E"]} {self.a["É"]} {self.a["R"]} {self.a["T"]} {self.a["Y"]} {self.a["Ý"]} {self.a["U"]} {self.a["Ú"]} {self.a["I"]} {self.a["Í"]} {self.a["O"]} {self.a["Ó"]} {self.a["P"]} {self.a["Ð"]}
-{SPACING} {self.a["A"]} {self.a["Á"]} {self.a["S"]} {self.a["D"]} {self.a["F"]} {self.a["G"]} {self.a["H"]} {self.a["J"]} {self.a["K"]} {self.a["L"]}
-{SPACING}  {self.a["Z"]} {self.a["X"]} {self.a["C"]} {self.a["V"]} {self.a["B"]} {self.a["N"]} {self.a["M"]}  {self.a["Þ"]}"""
-        keyboards = {"english": eng_kbd, "icelandic": ice_kbd}
-
-        return keyboards[self.language]
-        
-    def is_in_alphabet(self, letter):
-        return True if letter in self.a else False
-
 class Wordle:
     def __init__(self, no_rounds=6, letter_length=5) -> None:
         self.user = None
@@ -211,6 +120,7 @@ class Wordle:
 
         header = f"  {wins}   {rounds}   {streak}\n"
         header += f"╘{'═'*(len(wins)+2)}╧{'═'*(rounds_len//2)}{'─'*(rounds_len-(rounds_len//2)+2)}┴─────────····· ·  ·"
+        print(self.word)
 
         return header
 
@@ -277,7 +187,7 @@ class Wordle:
             for _ in range(self.rounds):
                 guess = self._prompt_guess()
                 self._compare_guess(guess)
-                if guess == self.word:
+                if guess == self.word.lower():
                     lost = False
                     self.wins += 1
                     self.streak += 1
--- wordleUI.py
+++ wordleUI.py
@@ -1,51 +1,158 @@
 import random
-import os
 from wordleLL import WordleLL
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
-def header():
-    print("█░█░█ █▀█ █▀█ █▀▄ █░░ █▀▀   █▀▀ ▄▀█ █▀▄▀█ █ █▄░█ █▀▀")
-    print("▀▄▀▄▀ █▄█ █▀▄ █▄▀ █▄▄ ██▄   █▄█ █▀█ █░▀░█ █ █░▀█ █▄█")
+from UIutils.Ui_utils import Screen
+from UIutils.Ui_utils import SPACING
+
 class Wordle:
     def __init__(self, user=None) -> None:
-        self.logic_wrapper = WordleLL()
-        self.user = user
+        self.logic_layer = WordleLL()
+        # Generate Word bank
+        self.word_bank = self.logic_layer.get_word_bank()
         self.word = None
-        self.word_bank = self.logic_wrapper.get_word_bank()
-        self.wins = 0
-        self.losses = 0
-        self.streak = 0
-        self.games_played = 0
-        self.prev_words = []
+
+        self.user = user
+        self.prev_words = None
         self.score = 0
         self.word_length = 0
         self.won = False
 
-    def initalize_word(self, length):
+        # Alphabet
+        self.a = None
+        
+        self.wins = 0
+        self.losses = 0
+        self.streak = 0
+        self.games_played = 0
+        self.cur_round = 0
+        self.rounds = 6
+
+        self.screen = Screen()
+
+
+
+    def initalize_game(self, length):
         self.won = False
-        self.prev_words = []
-        self.word = self.logic_wrapper.get_word(self.word_bank, length)
+        # Reseting the Alphabet
+        self.a = {"A": "A", 
+                  "B": "B", 
+                  "C": "C", 
+                  "D": "D", 
+                  "E": "E",
+                  "F": "F", 
+                  "G": "G", 
+                  "H": "H", 
+                  "I": "I", 
+                  "J": "J",
+                  "K": "K", 
+                  "L": "L", 
+                  "M": "M", 
+                  "N": "N", 
+                  "O": "O",
+                  "P": "P", 
+                  "Q": "Q", 
+                  "R": "R", 
+                  "S": "S", 
+                  "T": "T",
+                  "U": "U", 
+                  "V": "V", 
+                  "W": "W", 
+                  "X": "X", 
+                  "Y": "Y",
+                  "Z": "Z"}
+        self.cur_round = 0
+        self.prev_words = [""] * self.rounds
+        self.word = self.logic_layer.get_word(self.word_bank, length).lower()
         self.word_length = len(self.word)
 
-    def prompt_guess(self):
-        if self.prev_words:
-            print("Previously guessed words:")
-            print()
-            for word in self.prev_words:
-                print(word)
-            print()
+    def get_stats(self) -> str:
+        """
+        Returns a string of current win, round and streak stats
+        """
+        wins = f"Wins: {self.wins} / {self.games_played}"
+        rounds = f"Round: {min(self.cur_round+1, self.rounds)} / {self.rounds}"
+        rounds_len = len(rounds)
+        streak = f"Streak: \033[3{self.streak%8}m{self.streak}\033[0m"
+        score = f"Score: {self.score}"
 
-        guess = input("Make a guess: ").lower()
-        clear_screen()
-        header()
-        print()
+        header = f"  {wins}   {rounds}   {streak}\n"
+        header += f"╘{'═'*(len(wins)+2)}╧{'═'*(rounds_len//2)}{'─'*(rounds_len-(rounds_len//2)+2)}┴─────────····· ·  ·"
+
+        return header
+
+    def get_keyboard(self):
+        """
+        Returns the keyboard in the eng keyboard layout as a string
+        """
+        return f"""{SPACING}{self.a["Q"]} {self.a["W"]} {self.a["E"]} {self.a["R"]} {self.a["T"]} {self.a["Y"]} {self.a["U"]} {self.a["I"]} {self.a["O"]} {self.a["P"]}
+{SPACING} {self.a["A"]} {self.a["S"]} {self.a["D"]} {self.a["F"]} {self.a["G"]} {self.a["H"]} {self.a["J"]} {self.a["K"]} {self.a["L"]}
+{SPACING}  {self.a["Z"]} {self.a["X"]} {self.a["C"]} {self.a["V"]} {self.a["B"]} {self.a["N"]} {self.a["M"]}"""
+
+
+    def print_guesses(self, lost=False):
+        self.screen.clear_screen()
+
+        # Header
+        print(self.get_stats())
+
+        # Logo
+        print(f"""{SPACING}╦ ╦╔═╗╦═╗╔╦╗╦  ╔═
+{SPACING}╙╨╜╙─╜╨╙─ ╨╜╨─╜╙─""")
+        
+        # Printing each guessed word with fancy boarder
+        print(f"{SPACING} ╔══───··")
+        for i, word in enumerate(self.prev_words):
+            print(f"{SPACING} ║ {i + 1}. {word}")
+        print(f"{SPACING} ╚══╗", end="")
+
+        # Print the solution in red if game is lost
+        if lost: print(f" \033[31m{' '.join(self.word.upper())}\033[0m", end="")
+        print(f"\n{SPACING}    ╚══───···")
+    
+        # Keyboard visualization
+        print(self.get_keyboard())
         print(self.word)
+        print()
 
-        return self.check_guess(guess)
+
+
+    def prompt_guess(self):
+        self.print_guesses()
+        guess = input("Make a guess: ").lower()
+        self.screen.clear_screen()
+        print()
+
+
+        ret_str = ""
+        for i in range(len(guess)):
+            if guess[i] == self.word[i]:
+                # ret_str += f"\033[32m{guess[i].upper()}\033[0m "
+
+                # Coloring the keyboard letters green
+                self.a[guess[i].upper()] = f"\033[32m{guess[i].upper()}\033[0m"
+
+            elif guess[i] in self.word:
+                # ret_str += f"\033[33m{guess[i].upper()}\033[0m "
+
+                # Coloring a letter yellow, but not if a letter is already green
+                if self.a[guess[i].upper()] != f"\033[32m{guess[i].upper()}\033[0m":
+                    self.a[guess[i].upper()] = f"\033[33m{guess[i].upper()}\033[0m"
+
+            else:
+                # ret_str += f"\033[30m{guess[i].upper()}\033[0m "
+                
+                # Coloring the keyboard letters black
+                self.a[guess[i].upper()] = f"\033[30m{guess[i].upper()}\033[0m"
+
+        self.prev_words[self.cur_round] = ret_str
+        self.cur_round+=1
+
+        return guess
+
+        # return self.check_guess(guess)
 
     def check_guess(self, guess):
-        if self.logic_wrapper.validate_guess(guess, self.word):        
-            self.prev_words.append(guess)
+        if self.logic_layer.validate_guess(guess, self.word):        
+            self.prev_words[self.cur_round] = guess
             ret_string = ""
             
             for i in range(len(guess)):
@@ -59,15 +166,14 @@ class Wordle:
 
                 ret_string += "_ "
 
-            self.won = self.logic_wrapper.check_for_win(guess, self.word)
+            self.won = self.logic_layer.check_for_win(guess, self.word)
                 
             return ret_string + "\n"
 
     def play(self):
-        clear_screen()
-        header()
+        self.screen.clear_screen()
         user_input = int(input(f"Choose word length: "))
-        self.initalize_word(user_input)
+        self.initalize_game(user_input)
         self.guesses = 5
         
         while self.guesses >= 0:
@@ -75,13 +181,14 @@ class Wordle:
             print(guess)
             print(f"{self.guesses} guesses left.")
             self.guesses -=  1
-            if self.won:
+            if self.logic_layer.check_for_win(guess, self.word):
                 print("\nYou win!")
                 self.wins += 1
                 self.streak += 1
                 self.games_played += 1
                 self.score += self.guesses
                 return
+
             
         print("You lose")
         self.losses += 1
